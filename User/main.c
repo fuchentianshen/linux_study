f2b63990e85e1c55adefa9c4990de1310d1ada5b
WuHan <2749514521@qq.com>
2025-11-10 16:07:48 +0800

    ssh

--- User/main.c
+++ User/main.c
@@ -1,23 +1,23 @@
 #include "stdint.h"
 #include "stm32f10x.h"
-//´ò¿ªÈý¸öµÆ
+//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 int main(void)
 {
-	/* ¿ªÆôGPIOAµÄÊ±ÖÓ */
+	/* ï¿½ï¿½ï¿½ï¿½GPIOAï¿½ï¿½Ê±ï¿½ï¿½ */
 	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
 	//*(uint32_t *)(0x40021000 + 0x18) = 4;
 
-	/* ¸øPA0¡¢PA1ÉèÖÃÎªÍ¨ÓÃÍÆÍìÊä³ö */
+	/* ï¿½ï¿½PA0ï¿½ï¿½PA1ï¿½ï¿½ï¿½ï¿½ÎªÍ¨ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ */
 	GPIOA->CRL |=GPIO_CRL_MODE0;
 	GPIOA->CRL &= ~GPIO_CRL_CNF0;
 	GPIOA->CRL |=GPIO_CRL_MODE1;
 	GPIOA->CRL &= ~GPIO_CRL_CNF1;
 	//*(uint32_t *)(0x40010800 + 0x00) = 0x33;
-	//¸øPA8ÉèÖÃÎªÍ¨ÓÃÍÆÍìÊä³ö
+	//ï¿½ï¿½PA8ï¿½ï¿½ï¿½ï¿½ÎªÍ¨ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 	GPIOA->CRH |=GPIO_CRH_MODE8;
 	GPIOA->CRH &= ~GPIO_CRH_CNF8;
 	//*(uint32_t *)(0x40010800 + 0x04) = 0x03;
-	/* ¸øÊä³ö¼Ä´æÆ÷¸³Öµ */
+	/* ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä´ï¿½ï¿½ï¿½ï¿½ï¿½Öµ */
 	GPIOA->ODR &= ~GPIO_ODR_ODR0;
 	GPIOA->ODR &= ~GPIO_ODR_ODR1;
 	GPIOA->ODR &= ~GPIO_ODR_ODR8;
@@ -34,3 +34,4 @@ int main(void)
 
 //0000
 //1111
+//ssh
